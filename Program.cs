7e12f0d587b8032268fb65f391c5d198c7587a2a
Kevin Schelens Raymundo <kevin.raymundo@el.com.br>
2024-08-21 13:20:52 -0300

    melhoria no código

--- Program.cs
+++ Program.cs
@@ -4,31 +4,36 @@ using System.Text.Json;
 
 public class JsonEvaluator
 {
-    public static List<string> EvaluateJson(string jsonTemplate, string jsonToEvaluate)
+    public static (List<string> MissingFields, List<string> ExtraFields) EvaluateJson(string jsonTemplate, string jsonToEvaluate)
     {
         var missingFields = new List<string>();
+        var extraFields = new List<string>();
 
         using (JsonDocument templateDoc = JsonDocument.Parse(jsonTemplate))
         using (JsonDocument evaluateDoc = JsonDocument.Parse(jsonToEvaluate))
         {
-            CheckFields(templateDoc.RootElement, evaluateDoc.RootElement, "", missingFields);
+            CheckFields(templateDoc.RootElement, evaluateDoc.RootElement, "", missingFields, extraFields);
         }
 
-        return missingFields;
+        return (missingFields, extraFields);
     }
 
-    private static void CheckFields(JsonElement templateElement, JsonElement evaluateElement, string currentPath, List<string> missingFields)
+    private static void CheckFields(JsonElement templateElement, JsonElement evaluateElement, string currentPath, List<string> missingFields, List<string> extraFields)
     {
+        var templateFields = new HashSet<string>();
+
+        // Verifica os campos que estão presentes no template
         foreach (JsonProperty templateProperty in templateElement.EnumerateObject())
         {
             string fullPath = string.IsNullOrEmpty(currentPath) ? templateProperty.Name : $"{currentPath}.{templateProperty.Name}";
+            templateFields.Add(templateProperty.Name);
 
             if (evaluateElement.TryGetProperty(templateProperty.Name, out JsonElement evaluatedProperty))
             {
-                // Recursively check nested objects
+                // Verifica recursivamente objetos aninhados
                 if (templateProperty.Value.ValueKind == JsonValueKind.Object)
                 {
-                    CheckFields(templateProperty.Value, evaluatedProperty, fullPath, missingFields);
+                    CheckFields(templateProperty.Value, evaluatedProperty, fullPath, missingFields, extraFields);
                 }
             }
             else
@@ -36,82 +41,106 @@ public class JsonEvaluator
                 missingFields.Add(fullPath);
             }
         }
+
+        // Verifica os campos que estão presentes no JSON avaliado, mas não no template
+        foreach (JsonProperty evaluatedProperty in evaluateElement.EnumerateObject())
+        {
+            string fullPath = string.IsNullOrEmpty(currentPath) ? evaluatedProperty.Name : $"{currentPath}.{evaluatedProperty.Name}";
+
+            if (!templateFields.Contains(evaluatedProperty.Name))
+            {
+                extraFields.Add(fullPath);
+            }
+        }
     }
 
     public static void Main()
     {
         string jsonTemplate = @"{
-          ""portaL_ID"": 0,
-    ""hasH_CLIENTE"": ""string"",
-    ""boL_TRANSMITIR_REGISTRO"": ""s"",
-    ""seQ_GG_EMPRESA"": 0,
-    ""noM_EMPRESA"": ""string"",
-    ""nrO_EMPRESA_CNPJ"": ""string"",
-    ""seQ_GG_FILIAL"": 0,
-    ""noM_FILIAL"": ""string"",
-    ""nrO_FILIAL_CNPJ"": ""string"",
-    ""sbL_DIVIDA_FUNDADA"": ""s"",
-    ""noM_DIVIDA_FUNDADA_TIPO"": ""string"",
-    ""seQ_CT_UNIDADE_GESTORA"": 0,
-    ""seQ_CT_DIVIDA_FUNDADA"": 0,
-    ""noM_DIVIDA_TIPO_NATUREZA"": ""string"",
-    ""noM_DIVIDA_FUNDADA"": ""string"",
-    ""nrO_DIVIDA_FUNDADA_LEI"": ""string"",
-    ""anO_DIVIDA_FUNDADA_LEI"": ""stri"",
-    ""daT_DIVIDA_FUNDADA_LEI"": ""2024-08-21T09:54:30.107Z"",
-    ""noM_PESSOA"": ""string"",
-    ""nrO_DIVIDA_FUNDADA_PROCESSO"": ""string"",
-    ""anO_DIVIDA_FUNDADA_PROCESSO"": ""stri"",
-    ""nrO_DIVIDA_FUNDADA_CONTRATO"": ""string"",
-    ""daT_DIVIDA_FUNDADA_CONTRATO"": ""2024-08-21T09:54:30.107Z"",
-    ""daT_DIVIDA_FUNDADA_PUBLICACAO"": ""2024-08-21T09:54:30.107Z"",
-    ""anO_COMPETENCIA_DIVIDA_FUNDADA"": ""stri"",
-    ""ndA_COMPETENCIA"": ""string"",
-    ""stR_DIVIDA_FUNDADA_VEICULO_PUBLICACAO"": ""string"",
-    ""vlR_DIVIDA_FUNDADA_MOEDA_CONTRATADA"": 0,
-    ""vlR_DIVIDA_FUNDADA_MOEDA_CORRENTE"": 0,
-    ""vlR_DIVIDA_FUNDADA_SALDO"": 0,
-    ""hasH_REGISTRO_AREA"": ""string"",
-    ""controlE_AREA"": ""string""
+          ""id"": 0,
+    ""portal_Id"": 0,
+    ""hash_Registro"": ""string"",
+    ""hash_Cliente"": ""string"",
+    ""seq_Gg_Empresa"": 0,
+    ""nom_Empresa"": ""string"",
+    ""nro_Empresa_Cnpj"": ""string"",
+    ""seq_Gg_Filial"": 0,
+    ""nom_Filial"": ""string"",
+    ""nro_Filial_Cnpj"": ""string"",
+    ""seq_Ct_Convenio"": 0,
+    ""sbl_Convenio_Tipo"": ""string"",
+    ""nom_Convenio_Tipo"": ""string"",
+    ""sbl_Convenio_Subtipo"": ""string"",
+    ""nom_Convenio_Subtipo"": ""string"",
+    ""ano_Convenio"": 0,
+    ""nrd_Convenio"": ""string"",
+    ""nom_Convenio"": ""string"",
+    ""dat_Convenio"": ""2024-08-21T16:03:20.311Z"",
+    ""dat_Convenio_Inicio"": ""2024-08-21T16:03:20.311Z"",
+    ""dat_Convenio_Fim"": ""2024-08-21T16:03:20.311Z"",
+    ""mes_Convenio"": ""string"",
+    ""ano_Processo"": 0,
+    ""nrd_Processo"": ""string"",
+    ""seq_CtConvenio_Participante"": 0,
+    ""sbl_Pessoa_Participante_Tipo"": ""string"",
+    ""nom_Pessoa_Participante_Tipo"": ""string"",
+    ""sbl_Convenio_Legislacao"": ""string"",
+    ""nom_Convenio_Legislacao"": ""string"",
+    ""nro_Convenio_Legislacao"": ""string"",
+    ""seq_Gg_Pessoa"": 0,
+    ""nom_Pessoa"": ""string"",
+    ""nro_Pessoa_Cpf_Cnpj"": ""string"",
+    ""hst_Convenio"": ""strin"",
+    ""vlr_Convenio"": 0,
+    ""vlr_Convenio_Contrapartida"": 0,
+    ""anexo"": ""s"",
+    ""hash_Registro_Cpe"": ""string"",
+    ""controle_Cpe"": ""string""
         }";
 
         string jsonToEvaluate = @"{
-  
-    ""portaL_ID"": 0,
-    ""hasH_CLIENTE"": ""string"",
-    ""boL_TRANSMITIR_REGISTRO"": ""string"",
-    ""seQ_GG_EMPRESA"": 0,
-    ""noM_EMPRESA"": ""string"",
-    ""nrO_EMPRESA_CNPJ"": ""string"",
-    ""seQ_GG_FILIAL"": 0,
-    ""noM_FILIAL"": ""string"",
-    ""nrO_FILIAL_CNPJ"": ""string"",
-    ""sbL_DIVIDA_FUNDADA"": ""string"",
-    ""noM_DIVIDA_FUNDADA_TIPO"": ""string"",
-    ""seQ_CT_UNIDADE_GESTORA"": 0,
-    ""seQ_CT_DIVIDA_FUNDADA"": 0,
-    ""noM_DIVIDA_TIPO_NATUREZA"": ""string"",
-    ""noM_DIVIDA_FUNDADA"": ""string"",
-    ""nrO_DIVIDA_FUNDADA_LEI"": ""string"",
-    ""anO_DIVIDA_FUNDADA_LEI"": ""string"",
-    ""daT_DIVIDA_FUNDADA_LEI"": ""2024-08-21T09:54:44.300Z"",
-    ""noM_PESSOA"": ""string"",
-    ""nrO_DIVIDA_FUNDADA_PROCESSO"": ""string"",
-    ""anO_DIVIDA_FUNDADA_PROCESSO"": ""string"",
-    ""nrO_DIVIDA_FUNDADA_CONTRATO"": ""string"",
-    ""daT_DIVIDA_FUNDADA_CONTRATO"": ""2024-08-21T09:54:44.300Z"",
-    ""daT_DIVIDA_FUNDADA_PUBLICACAO"": ""2024-08-21T09:54:44.300Z"",
-    ""anO_COMPETENCIA_DIVIDA_FUNDADA"": ""string"",
-    ""ndA_COMPETENCIA"": ""string"",
-    ""stR_DIVIDA_FUNDADA_VEICULO_PUBLICACAO"": ""string"",
-    ""vlR_DIVIDA_FUNDADA_MOEDA_CONTRATADA"": 0,
-    ""vlR_DIVIDA_FUNDADA_MOEDA_CORRENTE"": 0,
-    ""vlR_DIVIDA_FUNDADA_SALDO"": 0,
-    ""hasH_REGISTRO_AREA"": ""string"",
-    ""controlE_AREA"": ""string""
-  }";
+          ""id"": 0,
+    ""portal_Id"": 0,
+    ""hash_Registro"": ""string"",
+    ""hash_Cliente"": ""string"",
+    ""seq_Gg_Empresa"": 0,
+    ""nom_Empresa"": ""string"",
+    ""nro_Empresa_Cnpj"": ""string"",
+    ""seq_Gg_Filial"": 0,
+    ""nom_Filial"": ""string"",
+    ""nro_Filial_Cnpj"": ""string"",
+    ""ano_Convenio"": 0,
+    ""nrd_Convenio"": ""string"",
+    ""nom_Convenio"": ""string"",
+    ""dat_Convenio"": ""2024-08-21T16:17:39.324Z"",
+    ""mes_Convenio"": ""string"",
+    ""ano_Processo"": 0,
+    ""nrd_Processo"": ""string"",
+    ""seq_CtConvenio_Participante"": 0,
+    ""sbl_Pessoa_Participante_Tipo"": ""string"",
+    ""nom_Pessoa_Participante_Tipo"": ""string"",
+    ""nro_Pessoa_Cpf_Cnpj"": ""string"",
+    ""hst_Convenio"": ""string"",
+    ""vlr_Convenio"": 0,
+    ""anexo"": ""string"",
+    ""hash_Registro_Cpe"": ""string"",
+    ""controle_Cpe"": ""string"",
+    ""sbl_Convenio_Tipo"": ""string"",
+    ""seq_Ct_Convenio"": 0,
+    ""nom_Convenio_Tipo"": ""string"",
+    ""sbl_Convenio_Subtipo"": ""string"",
+    ""nom_Convenio_Subtipo"": ""string"",
+    ""dat_Convenio_Inicio"": ""2024-08-21T16:17:39.324Z"",
+    ""dat_Convenio_Fim"": ""2024-08-21T16:17:39.324Z"",
+    ""sbl_Convenio_Legislacao"": ""string"",
+    ""nom_Convenio_Legislacao"": ""string"",
+    ""nro_Convenio_Legislacao"": ""string"",
+    ""seq_Gg_Pessoa"": 0,
+    ""nom_Pessoa"": ""string"",
+    ""vlr_Convenio_Contrapartida"": 0
+        }";
 
-        List<string> missingFields = EvaluateJson(jsonTemplate, jsonToEvaluate);
+        var (missingFields, extraFields) = EvaluateJson(jsonTemplate, jsonToEvaluate);
 
         if (missingFields.Count > 0)
         {
@@ -124,6 +153,19 @@ public class JsonEvaluator
         else
         {
             Console.WriteLine("Todos os campos estão presentes.");
+        }
+
+        if (extraFields.Count > 0)
+        {
+            Console.WriteLine("\nOs seguintes campos estão presentes no JSON avaliado, mas não no JSON padrão:");
+            foreach (string field in extraFields)
+            {
+                Console.WriteLine(field);
+            }
+        }
+        else
+        {
+            Console.WriteLine("\nNão há campos extras no JSON avaliado.");
         }
     }
 }
